ca6e9923fdd90b102111aa8ebf1265282f148fd1
Alan Gordon Reid <mail@alanreid.com.ar>
2016-10-19 03:13:49 +0200

    Changes the formatting of the log messages

--- drivers/localDisk.js
+++ drivers/localDisk.js
@@ -8,11 +8,9 @@ module.exports = function(config) {
   return {
     get: function(key, callback) {
 
-      winston.debug('LocalDisk: get ' + key);
+      winston.debug('LocalDisk: GET ' + key);
 
       var normalizedPath = require('path').join(config.localDisk.basePath, '..', key);
-
-      winston.debug('LocalDisk: normalizedPath ' + normalizedPath);
 
       fs.readFile(normalizedPath, function(err, data) {
           if(err) {
@@ -24,11 +22,9 @@ module.exports = function(config) {
 
     set: function(key, value, callback) {
 
-      winston.debug('LocalDisk: set ' + key);
+      winston.debug('LocalDisk: SET ' + key);
 
       var normalizedPath = require('path').join(config.localDisk.basePath, '..', key);
-
-      winston.debug('LocalDisk: normalizedPath ' + normalizedPath);
 
       fs.writeFile(normalizedPath, value, function(err) {
           if(err) {
--- drivers/memcached.js
+++ drivers/memcached.js
@@ -4,13 +4,13 @@ var Memcached = require('memcached'),
 module.exports = function(config) {
 
   winston.level = config.arcache.logLevel;
-  
+
   var memcached = new Memcached(config.memcached.hosts, config.memcached.options);
 
   return {
     get: function(key, callback) {
 
-      winston.debug('Memcached: get ' + key);
+      winston.debug('Memcached: GET ' + key);
 
       memcached.get(key, function(err, data) {
         if(err) {
@@ -22,7 +22,7 @@ module.exports = function(config) {
 
     set: function(key, value, callback) {
 
-      winston.debug('Memcached: set ' + key);
+      winston.debug('Memcached: SET ' + key);
 
       memcached.set(key, value, config.memcached.keyLifetime, function(err) {
         if(err) {
